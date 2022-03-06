bf817bc0266ce96e37a00cf04f4bcfb47be8b2c4
Francisco Moreno Cantero <fmoreno@marcoaldany.com>
2022-03-06 01:28:48 +0100

    docs: add example to all schemas

--- packages/adidas-schemas/src/index.js
+++ packages/adidas-schemas/src/index.js
@@ -1,27 +1,27 @@
 const Joi = require('joi');
 
 const id = Joi.object({
-  id: Joi.string().description('subscription\'s id')
+  id: Joi.string().description('subscription\'s id').example('62237e0f2ad6b25690243bae'),
 });
 
 const subscription = Joi.object({
-  id: Joi.string().description('subscription\'s id'),
-  email: Joi.string().email().description('user\'s email subscription'),
-  firstName: Joi.string().description('user name'),
-  gender: Joi.string().required().description('user\'s gender'),
-  dateOfBirth: Joi.date().required().description('user\'s gender'),
-  consent: Joi.boolean().required().description('send email'),
-  newsletterId: Joi.string().description('newsletter id'),
-  active: Joi.bool().default(true).description('subscription active status'),
+  id: Joi.string().description('subscription\'s id').example('62237e0f2ad6b25690243bae'),
+  email: Joi.string().email().description('customer\'s email subscription').example('mortadelo@tia.com'),
+  firstName: Joi.string().description('customer name').example('mortadelo'),
+  gender: Joi.string().required().description('customer\'s gender').example('male'),
+  dateOfBirth: Joi.date().required().description('customer\'s gender').example('11-28-1958'),
+  consent: Joi.boolean().required().description('send email').example(true),
+  newsletterId: Joi.string().description('newsletter id').example('123'),
+  active: Joi.bool().default(true).description('subscription active status').example(true),
 });
 
 const request = Joi.object({
-  id: Joi.string().description('subscription\'s id'),
+  id: Joi.string().description('subscription\'s id').example('62237e0f2ad6b25690243bae'),
 });
 
 const notification = Joi.object({
-  email: Joi.string().email().description('user\'s email subscription'),
-  newsletterId: Joi.string().description('newsletter id'),
+  email: Joi.string().email().description('customer\'s email subscription').example('mortadelo@tia.com'),
+  newsletterId: Joi.string().description('newsletter id').example('123'),
 });
 
 module.exports = {
