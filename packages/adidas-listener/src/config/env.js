75e5434e97792d5f8ba7a7a8d34e25da40d157fd
Francisco Moreno Cantero <fmoreno@marcoaldany.com>
2022-03-06 11:58:43 +0100

    style: sanitize env variables

--- packages/adidas-listener/src/config/env.js
+++ packages/adidas-listener/src/config/env.js
@@ -1,11 +1,11 @@
 module.exports = {
   mqtt: {
     uri: process.env.MQTT_URI || 'tcp://localhost:1883',
-    role: process.env.ROLE || 'selector',
+    role: (process.env.ROLE || 'selector').trim().toLocaleLowerCase(),
     topics: {
-      selector: process.env.MQTT_SUBSCRIPTIONS || 'subscriptions',
-      process: process.env.MQTT_PROCESS || 'process',
-      notify: process.env.MQTT_NOTIFY || 'email',
+      selector: (process.env.MQTT_SUBSCRIPTIONS || 'subscriptions').trim().toLowerCase(),
+      process: (process.env.MQTT_PROCESS || 'process').trim().toLowerCase(),
+      notify: (process.env.MQTT_NOTIFY || 'email').trim().toLowerCase(),
     },
   },
   apiUri: process.env.ADIDAS_API || 'http://localhost:8000',
